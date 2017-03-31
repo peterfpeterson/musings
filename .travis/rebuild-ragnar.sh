27332def87a59c4c4c088198498edc2d01309cea
Pete Peterson <peterfpeterson@gmail.com>
2017-03-31 16:44:46 -0400

    Don't always rebuild

--- .travis/rebuild-ragnar.sh
+++ .travis/rebuild-ragnar.sh
@@ -5,16 +5,16 @@ cd $direc/../docs
 pwd
 git checkout master
 
+CHANGED=false
+
 git diff --quiet AtlantaRagnar2017/data.json
 if [ $? ]; then
     echo Atlanta2017/data.json changed
     git add AtlantaRagnar2017/*
-else
-    git checkout AtlantaRagnar2017/plot.html
+    CHANGED=true
 fi
 
-git diff --cached --quiet
-if [ $? ]; then
+if [ "$CHANGED" = true ]; then
     git commit -m "Update from travis-ci build $TRAVIS_BUILD_NUMBER"
     git remote add origin-master https://${GITHUB_TOKEN}@github.com/peterfpeterson/musings.git > /dev/null 2>&1
     git push --set-upstream origin-master master
