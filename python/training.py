4ac93e4ec23dd513b531fb53088b80fe60744eec
Pete Peterson <peterfpeterson@gmail.com>
2018-11-27 11:53:46 -0500

    Add generators to create ical structure

--- python/training.py
+++ python/training.py
@@ -23,11 +23,15 @@ def standardize(stuff):
     stuff = stuff.replace('Half Marathon', 'Half marathon')
     return stuff
 
-def createFormatStr(training):
+def findLengths(training):
     lengths = [3,3,3,3,3,3,3]
     for week in training:
         for i, day in enumerate(week):
             lengths[i] = max(lengths[i], len(day))
+    return lengths
+
+def createFormatStr(training):
+    lengths = findLengths(training)
     lengths = ['{:' + str(length) + '}' for length in lengths]
     return ' '.join(lengths)
 
@@ -72,6 +76,24 @@ def toICalendar(training):
 
     calendar.add_component(event)
     return calendar
+
+def weekToICalGen(week, weeknum, weekdate):
+    startdate = date(weekdate.year, weekdate.month, weekdate.day)
+    for dayofweek, day in enumerate(week):
+        if day.strip() != REST.strip():
+            event = {'summary':day,
+                     'description':day,
+                     'dtstart':startdate + timedelta(days=dayofweek)}
+            if dayofweek == 0:
+                event['summary'] = 'Week {} - {}'.format(weeknum, day)
+            yield Event(**event)
+        else:
+            yield None
+
+def weekToTableGen(week, lengths):
+    lengths = ['{:' + str(length) + '}' for length in lengths]
+    for day, length in zip(week, lengths):
+        yield length.format(day)
 
 races = {'marathon': #https://www.halhigdon.com/training-programs/marathon-training/intermediate-2-marathon/
 '''
@@ -151,19 +173,27 @@ if __name__ == '__main__':
         print('training has already started, trimming to the last {} weeks'.format(int(weeks_util_race)+1))
         training = training[-1*int(weeks_util_race)-1:]
 
+    ical = None if not WITH_ICAL else Calendar()
+
     # print the results
-    frmt_week = createFormatStr(training)
-    print('{:19}'.format(''), frmt_week.format('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun').strip())
+    lengths = findLengths(training)
+    print('{:19}'.format(''), ' '.join(weekToTableGen(('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'), lengths)))
     for num, week in enumerate(training):
         weeknum = len(training) - num
         weekdate = raceweek - (weeknum - 1) * DELTA_WEEK
-        week = frmt_week.format(*week)
-        print("{:%Y-%m-%d} Week {:2}: {}".format(weekdate, weeknum, week.strip()))
 
-    if WITH_ICAL:
-        ical = toICalendar(training)
-        print('------------------------------')
-        print(ical)
+        # this creates the calendar
+        if ical is not None:
+            for day in weekToICalGen(week, weeknum, weekdate):
+                if day is not None:
+                    ical.add_component(day)
+
+        # this prints the table version
+        label = '{:%Y-%m-%d} Week {:2}:'.format(weekdate, weeknum)
+        print(label, ' '.join(weekToTableGen(week, lengths)))
+
+    # write the calendar to disk
+    if ical is not None:
         # write to disk
         filename = 'training.ics'
         with open(filename, 'wb') as handle:
